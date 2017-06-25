1fc377f71642e362b1fbd494425547ea63dcc466
Ritesh <riteshlogin@gmail.com>
2017-06-25 14:19:14 -0400

    Before brute force approach

--- src/Experiments/testGHT.cpp
+++ src/Experiments/testGHT.cpp
@@ -9,8 +9,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-
-    const char* filename = argc >= 2 ? argv[1] : "testGeneralHough.png";
+    const char* filename = argc >= 2 ? argv[1] : "pic1.png";
 
     cv::Mat src = imread(filename, IMREAD_GRAYSCALE);
     Mat roomba_canny, hsv_image;
@@ -20,26 +19,33 @@ int main(int argc, char** argv)
     // Basing my usage off of the example here (https://github.com/opencv/opencv/blob/master/samples/gpu/generalized_hough.cpp)
     // And what I could glean from reading the generalHough code on the 2.4 branch in the opencv repository.
 
-    Ptr< GeneralizedHough > generalHough =  cv::GeneralizedHough::create(GHT_POSITION | GHT_ROTATION | GHT_SCALE);
+    int method = GHT_POSITION + GHT_ROTATION + GHT_SCALE;
 
-    cv::Mat roomba_template = imread("blackOutline.png", IMREAD_GRAYSCALE);
+    Ptr< GeneralizedHough > generalHough =  generalHough->create(method);
+    generalHough->set("angleThresh", 10000);
+    generalHough->set("scaleThresh", 1000);
+    generalHough->set("posThresh", 100);
+    generalHough->set("maxSize", 1000);
+    generalHough->set("levels", 360);
+    generalHough->set("minAngle", 0);
+    generalHough->set("maxAngle", 360);
+    generalHough->set("angleStep", 1);
+    generalHough->set("minScale", 0.5);
+    generalHough->set("maxScale", 2);
+    generalHough->set("dp", 2);
+    
+    // generalHough->set("levels", 100);
+
+    cv::Mat roomba_template = imread("templ.png", IMREAD_GRAYSCALE);
 
     cout << roomba_template.type()  << std::endl;
 
-    roomba_template.convertTo(roomba_template, CV_8UC1);
-
-    cout << roomba_template.type() << std::endl;
-
-    generalHough->setTemplate(roomba_template, 100);
+    generalHough->setTemplate(roomba_template);
 
     vector<Vec4f> roomba_positions;
     vector<Vec3i> roomba_votes;
 
-    // Canny(src, roomba_canny, 100, 50);
-
-    // imshow("canny output", roomba_canny);
-
-    generalHough->detect(src, roomba_positions, roomba_votes, 100);
+    generalHough->detect(src, roomba_positions);
 
     if(roomba_positions.size() == 0)
     {
@@ -48,7 +54,7 @@ int main(int argc, char** argv)
 
     for (int i = 0; i < roomba_positions.size(); i++)
     {
-        cv::rectangle(src, Point2i(roomba_positions[i][0], roomba_positions[i][1]), Point2i(roomba_positions[i][0] + 50, roomba_positions[i][1] + 50) , Scalar( 80, 180, 180 ), 6 , CV_AA);
+        cv::rectangle(src, Point2i(roomba_positions[i][0]-50, roomba_positions[i][1]-50), Point2i(roomba_positions[i][0] + 50, roomba_positions[i][1] + 50) , Scalar( 100, 180, 180 ), 6 , CV_AA);
     }
 
     imshow("Output", src);
