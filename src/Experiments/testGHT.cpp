88908eb0cb1d750d7569d91517849fd9bb95e00a
Ritesh <riteshlogin@gmail.com>
2017-06-25 14:50:32 -0400

    Attempt to use opencv sample code
    It doesn't detect the most simple rotation test I could make. Is this worth using?

--- src/Experiments/testGHT.cpp
+++ src/Experiments/testGHT.cpp
@@ -1,65 +1,158 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-
+#include <vector>
 #include <iostream>
+#include <string>
 
-using namespace cv;
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/gpu/gpu.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+
 using namespace std;
+using namespace cv;
+using namespace cv::gpu;
 
-
-int main(int argc, char** argv)
+static Mat loadImage(const string& name)
 {
-    const char* filename = argc >= 2 ? argv[1] : "pic1.png";
-
-    cv::Mat src = imread(filename, IMREAD_GRAYSCALE);
-    Mat roomba_canny, hsv_image;
-    
-    cout << "test" << std::endl;
-
-    // Basing my usage off of the example here (https://github.com/opencv/opencv/blob/master/samples/gpu/generalized_hough.cpp)
-    // And what I could glean from reading the generalHough code on the 2.4 branch in the opencv repository.
-
-    int method = GHT_POSITION + GHT_ROTATION + GHT_SCALE;
-
-    Ptr< GeneralizedHough > generalHough =  generalHough->create(method);
-    generalHough->set("angleThresh", 10000);
-    generalHough->set("scaleThresh", 1000);
-    generalHough->set("posThresh", 100);
-    generalHough->set("maxSize", 1000);
-    generalHough->set("levels", 360);
-    generalHough->set("minAngle", 0);
-    generalHough->set("maxAngle", 360);
-    generalHough->set("angleStep", 1);
-    generalHough->set("minScale", 0.5);
-    generalHough->set("maxScale", 2);
-    generalHough->set("dp", 2);
-    
-    // generalHough->set("levels", 100);
-
-    cv::Mat roomba_template = imread("templ.png", IMREAD_GRAYSCALE);
-
-    cout << roomba_template.type()  << std::endl;
-
-    generalHough->setTemplate(roomba_template);
-
-    vector<Vec4f> roomba_positions;
-    vector<Vec3i> roomba_votes;
-
-    generalHough->detect(src, roomba_positions);
-
-    if(roomba_positions.size() == 0)
+    Mat image = imread(name, IMREAD_GRAYSCALE);
+    if (image.empty())
     {
-        cout << "nooooo" << std::endl;
+        cerr << "Can't load image - " << name << endl;
+        exit(-1);
+    }
+    return image;
+}
+
+int main(int argc, const char* argv[])
+{
+    CommandLineParser cmd(argc, argv,
+        "{ i | image          | pic1.png  | input image }"
+        "{ t | template       | templ.png | template image }"
+        "{ s | scale          |           | estimate scale }"
+        "{ r | rotation       |           | estimate rotation }"
+        "{   | gpu            |           | use gpu version }"
+        "{   | minDist        | 100       | minimum distance between the centers of the detected objects }"
+        "{   | levels         | 360       | R-Table levels }"
+        "{   | votesThreshold | 30        | the accumulator threshold for the template centers at the detection stage. The smaller it is, the more false positions may be detected }"
+        "{   | angleThresh    | 10000     | angle votes treshold }"
+        "{   | scaleThresh    | 1000      | scale votes treshold }"
+        "{   | posThresh      | 100       | position votes threshold }"
+        "{   | dp             | 2         | inverse ratio of the accumulator resolution to the image resolution }"
+        "{   | minScale       | 0.5       | minimal scale to detect }"
+        "{   | maxScale       | 2         | maximal scale to detect }"
+        "{   | scaleStep      | 0.05      | scale step }"
+        "{   | minAngle       | 0         | minimal rotation angle to detect in degrees }"
+        "{   | maxAngle       | 360       | maximal rotation angle to detect in degrees }"
+        "{   | angleStep      | 1         | angle step in degrees }"
+        "{   | maxSize        | 1000      | maximal size of inner buffers }"
+        "{ h | help           |           | print help message }"
+    );
+
+    //cmd.about("This program demonstrates arbitary object finding with the Generalized Hough transform.");
+
+    if (cmd.get<bool>("help"))
+    {
+        cmd.printParams();
+        return 0;
     }
 
-    for (int i = 0; i < roomba_positions.size(); i++)
+    const string templName = cmd.get<string>("template");
+    const string imageName = cmd.get<string>("image");
+    bool estimateScale = cmd.get<bool>("scale");
+    bool estimateRotation = cmd.get<bool>("rotation");
+    const bool useGpu = cmd.get<bool>("gpu");
+    const double minDist = cmd.get<double>("minDist");
+    const int levels = cmd.get<int>("levels");
+    const int votesThreshold = cmd.get<int>("votesThreshold");
+    const int angleThresh = cmd.get<int>("angleThresh");
+    const int scaleThresh = cmd.get<int>("scaleThresh");
+    const int posThresh = cmd.get<int>("posThresh");
+    const double dp = cmd.get<double>("dp");
+    const double minScale = cmd.get<double>("minScale");
+    const double maxScale = cmd.get<double>("maxScale");
+    const double scaleStep = cmd.get<double>("scaleStep");
+    const double minAngle = cmd.get<double>("minAngle");
+    const double maxAngle = cmd.get<double>("maxAngle");
+    const double angleStep = cmd.get<double>("angleStep");
+    const int maxSize = cmd.get<int>("maxSize");
+
+    Mat templ = loadImage(templName);
+    Mat image = loadImage(imageName);
+    estimateScale = true;
+    estimateRotation = true;
+
+    int method = GHT_POSITION;
+    if (estimateScale)
+        method += GHT_SCALE;
+    if (estimateRotation)
+        method += GHT_ROTATION;
+
+    vector<Vec4f> position;
+
     {
-        cv::rectangle(src, Point2i(roomba_positions[i][0]-50, roomba_positions[i][1]-50), Point2i(roomba_positions[i][0] + 50, roomba_positions[i][1] + 50) , Scalar( 100, 180, 180 ), 6 , CV_AA);
+        Ptr<GeneralizedHough> hough = GeneralizedHough::create(method);
+        hough->set("minDist", minDist);
+        hough->set("levels", levels);
+        hough->set("dp", dp);
+        if (estimateScale && estimateRotation)
+        {
+            hough->set("angleThresh", angleThresh);
+            hough->set("scaleThresh", scaleThresh);
+            hough->set("posThresh", posThresh);
+            hough->set("maxSize", maxSize);
+        }
+        else
+        {
+            hough->set("votesThreshold", votesThreshold);
+        }
+        if (estimateScale)
+        {
+            cout << "trying scale\n";
+            hough->set("minScale", minScale);
+            hough->set("maxScale", maxScale);
+            hough->set("scaleStep", scaleStep);
+        }
+        if (estimateRotation)
+        {
+            cout << "trying rotation\n";
+            hough->set("minAngle", minAngle);
+            hough->set("maxAngle", maxAngle);
+            hough->set("angleStep", angleStep);
+        }
+
+        hough->setTemplate(templ);
+
+        hough->detect(image, position);
+
     }
 
-    imshow("Output", src);
+    cout << "Found : " << position.size() << " objects" << endl;
+
+    Mat out;
+    cvtColor(image, out, COLOR_GRAY2BGR);
+
+    for (size_t i = 0; i < position.size(); ++i)
+    {
+        Point2f pos(position[i][0], position[i][1]);
+        float scale = position[i][2];
+        float angle = position[i][3];
+
+        RotatedRect rect;
+        rect.center = pos;
+        rect.size = Size2f(templ.cols * scale, templ.rows * scale);
+        rect.angle = angle;
+
+        Point2f pts[4];
+        rect.points(pts);
+
+        line(out, pts[0], pts[1], Scalar(0, 0, 255), 3);
+        line(out, pts[1], pts[2], Scalar(0, 0, 255), 3);
+        line(out, pts[2], pts[3], Scalar(0, 0, 255), 3);
+        line(out, pts[3], pts[0], Scalar(0, 0, 255), 3);
+    }
+
+    imshow("out", out);
     waitKey();
 
     return 0;
-
 }
